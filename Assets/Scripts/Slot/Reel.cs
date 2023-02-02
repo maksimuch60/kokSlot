307b94386bdf2a097de457ace0be316ddfac03a7
Maksim Anishchenko <maksimych60@gmail.com>
2023-02-02 23:07:27 +0300

    starting reel logic

---
+++ Assets/Scripts/Slot/Reel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Slot
+{
+    public class Reel : MonoBehaviour
+    {
+        [SerializeField] private List<Slot> _reelSlots;
+        [SerializeField] private float _spinSpeed;
+        
+        
+        
+    }
+}
---
+++ Assets/Scripts/Slot/Slot.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Slot
+{
+    public class Slot : MonoBehaviour
+    {
+        [SerializeField] private Image _slotImage;
+        
+        
+    }
+}
